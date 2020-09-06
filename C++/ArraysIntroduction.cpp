5e3926db75b568556f9a2e5b15fc6318683424e8
Tuna Tuncer <66808459+preroot@users.noreply.github.com>
2020-09-06 13:10:42 +0300

    Update ArraysIntroduction.cpp

--- C++/ArraysIntroduction.cpp
+++ C++/ArraysIntroduction.cpp
@@ -1,30 +1,44 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <sstream>
 using namespace std;
 
-
-
 int main() {
-    int N;
-    cin >> N;
-    cin.ignore();
-    string input;
-    getline(cin, input);
-    istringstream iss(input);
-    int i;
-    vector<int> vec;
-    while (iss >> i) {
-        vec.push_back(i);
-    }
-    reverse(vec.begin(), vec.end());
-    for (size_t i = 0; i < vec.size(); i++) {
-        cout << vec.at(i) << " "; 
-    }
+	int N;
+	// This variable is for length of the array, but in the end I decided not to use it at all
 
-    return 0;
+	cin >> N;
+	cin.ignore();
+	// We need to use this method or we cannot get input with getline method.
+
+	string input;
+	// The getline method takes the input as a string, so our variable type should be string.
+
+	getline(cin, input);
+	istringstream iss(input);
+	// Object class of istringstream 
+
+	int i;
+	// To store integer values to be streamed from istringstream
+
+	vector<int> vec;
+	// I finally decided to use a vector instead of an array
+	// because when I used the input to create the size of the array, I got an error and couldn't find an easy way to solve it.
+
+	while (iss >> i) {
+		vec.push_back(i);
+	}
+	// I did the transfer of integer values from string to an integer variable with the extraction operator.
+	// The extraction operator will read until whitespace is reached or until the stream fails.
+
+	reverse(vec.begin(), vec.end());
+	// You can check https://www.geeksforgeeks.org/stdreverse-in-c/ to understand what this line is doing.
+
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec.at(i) << " "; 
+	}
+	//Finally, I printed it in a line with spaces between values
+
+	return 0;
 }
